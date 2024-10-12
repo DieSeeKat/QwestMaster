169fc8ff9215c9c350e90fee55c4f893eb0c2f89
Lukas Anthonissen <lukasanthonissen@gmail.com>
2024-10-12 15:28:29 +0200

    Add env for baseURL

--- src/app/quest/[slug]/hint.tsx
+++ src/app/quest/[slug]/hint.tsx
@@ -1,12 +1,14 @@
 export default async function Hint({ params } : { params: { slug: string }}) {
 
+    const baseURL = process.env.BASE_URL || 'http://localhost:3000';
+
     let data = {
         hint: "",
         image: false
     };
 
     try {
-        data = await fetch(`http://localhost:3000/api/quest/${params.slug}`).then(res => res.json());
+        data = await fetch(`${baseURL}/api/quest/${params.slug}`).then(res => res.json());
 
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
